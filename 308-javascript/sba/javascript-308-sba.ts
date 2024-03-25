0747f2389381b90b02235101064c1882ef63ab43
chadcito <145295470+chadcito@users.noreply.github.com>
2024-03-25 15:37:38 -0600

    get unique learner ids

--- 308-javascript/sba/javascript-308-sba.ts
+++ 308-javascript/sba/javascript-308-sba.ts
@@ -7,17 +7,29 @@ import {
   LearnerSubmission,
 } from './data';
 
-type LearnerData = {
-  id: number;
-  avg: number;
-  assignment_id: number;
-};
+
+
+const getUniqueLearners = (learners: Array<LearnerSubmission>): Array<number> =>
+  learners
+    .map((learner) => learner.learner_id)
+    .filter((id, i, curr) => curr.indexOf(id) === i);
+
+
 
 const getLearnerData = (
   courseInfo: CourseInfo,
   assignmentGroup: AssignmentGroup,
-  leanerSubmissions: Array<LearnerSubmission>
-) => {};
+  learnerSubmissions: Array<LearnerSubmission>
+) => {
+  try {
+    if (assignmentGroup.course_id != courseInfo.id)
+      throw Error('Assignment Group and Course IDs do not match');
+
+    const uniqueLearnerIds = getUniqueLearners(learnerSubmissions);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const learnerData = getLearnerData(
   COURSE_INFO_DATA,
@@ -25,4 +37,34 @@ const learnerData = getLearnerData(
   LEARNER_SUBMISSIONS_DATA
 );
 
-console.log(learnerData);
+console.log('LEARNER DATA ', learnerData);
+
+// const mappedLearners = sortedLearners.map((learnerData) => {
+//   //   console.log(learner);
+//   const learnerResults = new Map();
+//   const numeratorWeight: Array<number> = [];
+//   const denomninatroWeight: Array<number> = [];
+
+//   learnerResults.set('id', learnerData[0].learner_id);
+//   learnerData.forEach((assignment) => {
+//     const { assignment_id, submission } = assignment;
+
+//     for (let i = 0; i < assignments.length; i++) {
+//       if (assignment_id == assignments[i].id) {
+//         console.log(
+//           'ASSIGNMENT ',
+//           submission.score,
+//           assignments[i].points_possible
+//         );
+//         const assignmentAvg =
+//           submission.score / assignments[i].points_possible;
+//         learnerResults.set(assignment_id, assignmentAvg);
+//         numeratorWeight.push(submission.score);
+//         denomninatroWeight.push(assignments[i].points_possible);
+//       }
+//     }
+//   });
+//   learnerResults.set('avg', calcAvg(numeratorWeight, denomninatroWeight));
+//   console.log(calcAvg(numeratorWeight, denomninatroWeight));
+//   return learnerResults;
+// });
