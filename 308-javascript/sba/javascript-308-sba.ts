6733d0e704a2135b49882ad39db112585cfff063
chadcito <145295470+chadcito@users.noreply.github.com>
2024-03-25 15:48:18 -0600

    calculated averages and mapped learners to new object structure

--- 308-javascript/sba/javascript-308-sba.ts
+++ 308-javascript/sba/javascript-308-sba.ts
@@ -7,6 +7,7 @@ import {
   LearnerSubmission,
 } from './data';
 
+
 const getUniqueLearners = (learners: Array<LearnerSubmission>): Array<number> =>
   learners
     .map((learner) => learner.learner_id)
@@ -27,6 +28,41 @@ const sortLearnersByID = (
   return sortedLearners;
 };
 
+const calcAvg = (numArr: Array<number>, denomArr: Array<number>): number => {
+  const numerator = numArr.reduce((acc, curr) => acc + curr, 0);
+  const denominator = denomArr.reduce((acc, curr) => acc + curr, 0);
+
+  return numerator / denominator;
+};
+
+const mapLearners = (
+  sortedLearners: Array<Array<LearnerSubmission>>,
+  assignments
+) =>
+  sortedLearners.map((learnerData: Array<LearnerSubmission>) => {
+    const learnerResults = {};
+    const numeratorWeight: Array<number> = [];
+    const denomninatroWeight: Array<number> = [];
+
+    learnerResults['id'] = learnerData[0].learner_id;
+
+    learnerData.forEach((assignment) => {
+      const { assignment_id, submission } = assignment;
+
+      for (let i = 0; i < assignments.length; i++) {
+        if (assignment_id == assignments[i].id) {
+          const assignmentAvg =
+            submission.score / assignments[i].points_possible;
+          learnerResults[assignment_id] = assignmentAvg;
+          numeratorWeight.push(submission.score);
+          denomninatroWeight.push(assignments[i].points_possible);
+        }
+      }
+    });
+    learnerResults['avg'] = calcAvg(numeratorWeight, denomninatroWeight);
+
+    return learnerResults;
+  });
 
 const getLearnerData = (
   courseInfo: CourseInfo,
@@ -43,6 +79,11 @@ const getLearnerData = (
       uniqueLearnerIds,
       learnerSubmissions
     );
+
+    const { assignments } = assignmentGroup;
+
+    const mappedLearners: Array<any> = mapLearners(sortedLearners, assignments);
+    return mappedLearners;
   } catch (error) {
     console.log(error);
   }
@@ -53,3 +94,5 @@ const learnerData = getLearnerData(
   ASSIGNMENT_GROUP_DATA,
   LEARNER_SUBMISSIONS_DATA
 );
+
+console.log('LEARNER DATA ', learnerData);
