f295a675e85d959118d5c54af41f01695f15b805
chadcito <145295470+chadcito@users.noreply.github.com>
2024-03-27 21:22:57 -0600

    added game finish and retreat functionality

--- 308-javascript/space-battle/public/scripts/space-battle.ts
+++ 308-javascript/space-battle/public/scripts/space-battle.ts
@@ -17,21 +17,47 @@ const alienCount: number = 5;
 const CAPTAIN_HIT_DIALOG: string = 'Yes! Direct Hit!';
 const CAPTAIN_MISS_DIALOG: string = 'Rats! We missed!';
 const CAPTAIN_DESTROY_DIALOG: string = 'Another one bites the dust!';
+const GAME_WON_TITLE = 'Congratulations Captain';
+const GAME_WON_SLOGAN =
+  "Call me cocky, but if there's an alien out there I can't kill, I haven't met him and killed him yet.";
+const GAME_LOST_TITLE = 'DOOOOOOOOMMMMMM!!!';
+const GAME_RETREAT_TITLE = 'COWARDS!!';
+const GAME_RETREAT_SLOGAN =
+  'My instincts are to hide in the barrel like the wiley fish.';
+
+const ZAPP_QUOTE_1 = "When I'm In Command, Every Mission Is A Suicide Mission.";
+const ZAPP_QUOTE_2 =
+  'The Spirit Is Willing, But The Flesh Is Spongy And Bruised.';
+('Fire all weapons and open a hailing frequency for my victory yodel.');
+('Has my reputation preceded me or was I too quick for it?');
+("Call me cocky, but if there's an alien out there I can't kill, I haven't met him and killed him yet.");
+("Don't blame yourself, Kif. We were doomed from the start. Nothing remains now but for the captain to go down with his ship.");
+('My instincts are to hide in the barrel like the wiley fish.');
+('The alien mothership is in orbit here. If we can hit that bullseye, the rest of the dominoes will fall like a house of cards. Checkmate. ');
+(' I surrender and volunteer for treason.');
+("She's built like a steakhouse but she handles like a bistro.");
+('Stop exploding you cowards!!');
+('We Know Nothing About Them, Their Language, Their History, Or What They Look Like. But We Can Assume This. They Stand For Everything We Don’t Stand For. Also, They Told Me You Guys Look like cowards');
+
+// LEELA
+("Look, I don't know about your previous captains, but I intend to do as little dying as possible");
 
 const OMICRON_HIT_DIALOG: string = 'Hit!! Soon you will meet your fate!';
 const OMICRON_MISS_DIALOG: string = 'Miss? Unacceptable! Recalibrate lasers!';
 const OMICRON_DESTROY_DIALOG: string = 'MUAH AHAH AH! Doooom!';
 
+const backdrop = document.getElementById('backdrop') as HTMLDivElement;
 const gameArea = document.getElementById('game-area') as HTMLDivElement;
 const selectCaptainModal = document.getElementById(
   'captain-modal'
 ) as HTMLDivElement;
-const selectCaptain = document.querySelector('.captain__container');
-console.log(selectCaptain);
 const selectShipModal = document.getElementById('ship-modal') as HTMLDivElement;
+const gameFinishModal = document.getElementById(
+  'game-finish-modal'
+) as HTMLDivElement;
+
+const selectCaptain = document.querySelector('.captain__container');
 const selectShip = document.querySelector('.ship__container');
-console.log('SHIP ', selectShip);
-const backdrop = document.getElementById('backdrop') as HTMLDivElement;
 
 const captain = document.getElementById('captain') as HTMLDivElement;
 const omicronShipCount = document.getElementById(
@@ -47,19 +73,22 @@ const omicronHull = document.getElementById(
 ) as HTMLProgressElement;
 const captainHullTitle: HTMLElement =
   document.getElementById('captain-hull-title');
+
 const captainBubble: HTMLElement = document.getElementById('captain-bubble');
 const omicronBubble: HTMLElement = document.getElementById('omicron-bubble');
 
-const attackBtn: HTMLElement = document.getElementById('attack-btn');
+const attackBtn = document.getElementById('attack-btn') as HTMLButtonElement;
+const retreatBtn = document.getElementById('retreat-btn') as HTMLButtonElement;
 
-const toggleBackdrop = (): void => backdrop.classList.remove('visible');
+const toggleBackdrop = (): void => {
+  backdrop.classList.toggle('visible');
+};
 
 const toggleCaptainModal = (): void =>
   selectCaptainModal.classList.remove('visible');
 
 const toggleShipModal = (): void => {
   selectShipModal.classList.add('visible');
-  toggleBackdrop();
 };
 
 const selectCaptainHandler = (e: InputEvent): void => {
@@ -83,13 +112,14 @@ const selectShipHandler = (e: InputEvent): void => {
     captainImg.setAttribute('alt', 'Zapp');
   }
   if (shipName === 'nimbus-select') {
-    const ship = captain.children[1].firstElementChild as HTMLImageElement;
-    ship.setAttribute('src', 'images/nimbus.webp');
-    ship.setAttribute('alt', 'Nimbus');
-    ship.style.height = '320px';
-    captainHullTitle.innerText = 'Nimbus Hull';
+    const shipImg = captain.children[1].firstElementChild as HTMLImageElement;
+    shipImg.setAttribute('src', 'images/nimbus.webp');
+    shipImg.setAttribute('alt', 'Nimbus');
+    shipImg.style.height = '320px';
+    captainHullTitle.textContent = 'Nimbus Hull';
   }
-  selectShipModal.classList.remove('visible');
+  selectShipModal.classList.toggle('visible');
+  toggleBackdrop();
   gameArea.classList.remove('opacity');
   setOmicronHull(omicronFleet[omicronFleet.length - 1].hull);
 };
@@ -115,7 +145,7 @@ const setOmicronHull = (hullValue) => {
 };
 
 const speak = (element: HTMLElement, dialog: string): void => {
-  element.innerText = dialog;
+  element.textContent = dialog;
   element.style.opacity = '1';
   setTimeout(() => {
     element.style.opacity = '0';
@@ -130,24 +160,51 @@ const fireLasers = (player: HTMLDivElement): void => {
   setTimeout(() => ship.classList.remove('fire'), 600);
 };
 
+const gameFinish = (action: string): void => {
+  const content = gameFinishModal.firstElementChild as HTMLDivElement;
+  const title = content.firstElementChild as HTMLDivElement;
+  const gameFinishImage = content.children[1] as HTMLImageElement;
+  const slogan = content.children[2] as HTMLDivElement;
+  if (action == 'retreat') {
+    title.textContent = GAME_RETREAT_TITLE;
+    gameFinishImage.setAttribute('src', 'images/zapp-fish-full.webp');
+    gameFinishImage.setAttribute('alt', 'The Wiley Fish');
+    slogan.textContent = GAME_RETREAT_SLOGAN;
+    gameFinishModal.classList.toggle('visible');
+  } else if (action == 'lose') {
+    title.textContent = GAME_LOST_TITLE;
+    gameFinishImage.setAttribute('src', 'images/lrrr-doom.webp');
+    gameFinishImage.setAttribute('alt', 'Doooom!');
+    gameFinishImage.style.width = '300px';
+    slogan.textContent = '';
+    gameFinishModal.classList.toggle('visible');
+  } else {
+    gameFinishModal.classList.toggle('visible');
+  }
+  toggleBackdrop();
+};
+
 const subtractDamage = (
   offense: Ship,
   defense: Ship,
   defenseHull: HTMLProgressElement
-) => {
+): number => {
   defense.hull -= offense.firepower;
   defenseHull.value = defense.hull;
+  return defense.hull;
 };
 
 const omicronAttack = (): void => {
   if (isHit(omicronFleet[omicronFleet.length - 1].accuracy)) {
     fireLasers(omicron);
-    subtractDamage(
+    const enemyHull: number = subtractDamage(
       omicronFleet[omicronFleet.length - 1],
       captainShip,
       captainHull
     );
-    speak(omicronBubble, OMICRON_HIT_DIALOG);
+    enemyHull <= 0
+      ? gameFinish('lose')
+      : speak(omicronBubble, OMICRON_HIT_DIALOG);
   } else {
     speak(omicronBubble, OMICRON_MISS_DIALOG);
   }
@@ -172,12 +229,29 @@ const attack = (): void => {
       setTimeout(omicronAttack, 500);
     } else {
       omicronFleet.pop();
-      speak(captainBubble, CAPTAIN_DESTROY_DIALOG);
-      omicronShipCount.innerText = omicronFleet.length.toString();
-      setOmicronHull(omicronFleet[omicronFleet.length - 1].hull);
+      if (omicronFleet.length <= 0) {
+        gameFinish('win');
+      } else {
+        speak(captainBubble, CAPTAIN_DESTROY_DIALOG);
+        retreatBtn.classList.remove('push--disabled');
+        retreatBtn.classList.add('push--flat');
+        omicronShipCount.textContent = omicronFleet.length.toString();
+        setOmicronHull(omicronFleet[omicronFleet.length - 1].hull);
+      }
     }
   }
 };
+
+const retreatHandler = (e) => {
+  gameFinish('retreat');
+};
+
+const reset = (e: InputEvent) => {
+  if (e.target instanceof HTMLButtonElement) location.reload();
+};
+
 selectCaptain.addEventListener('click', selectCaptainHandler);
 selectShip.addEventListener('click', selectShipHandler);
 attackBtn.addEventListener('click', attack);
+retreatBtn.addEventListener('click', retreatHandler);
+gameFinishModal.addEventListener('click', reset);
